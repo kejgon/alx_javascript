ef0440628e3a4f5fb3fc9863001c682cb7922e52
kejgon james <kejgon@gmail.com>
2024-02-06 19:44:12 +0300

    commit

---
+++ Js Test/network.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const endpoint = 'https://swapi.dev/api/people/1'
+
+//http://127.0.0.1:5000/people/1 - flask server
+
+// request('http://www.google.com', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+
+request(endpoint, function () {
+
+})
+
