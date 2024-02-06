9efa4e73a1c1c5c510ba48fb76528a1e6b5be9de
kejgon james <kejgon@gmail.com>
2024-02-06 19:31:05 +0300

    commit

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,25 +1,30 @@
 #!/usr/bin/node
 
 const myObject = {
-    type: 'object',
-    value: 12,
-    incr: function () {
+    type: 'object',     // String property 'type' with value 'object'.
+    value: 12,          // Numeric property 'value' with initial value 12.
+    incr: function () { // Method 'incr': a function that increments the 'value' property by 1.
         this.value += 1;
     }
 };
 
-// Manually format the output for the first object
+// Formatting of the First Object Output:
 const firstObjectOutput = `{ type: '${myObject.type}', value: ${myObject.value} }`;
 
+// Outputing of the First Object:
 console.log(firstObjectOutput);
 
+// Display Object Function:
 function displayObject(obj) {
     console.log(`{ type: '${obj.type}', value: ${obj.value}, incr: [Function] }`);
 }
 
-myObject.incr();
-displayObject(myObject);
-myObject.incr();
-displayObject(myObject);
-myObject.incr();
-displayObject(myObject);
+// Calling incr Method and Displaying the Object:
+myObject.incr();        // Incrementing 'value' property.
+displayObject(myObject); // Displaying the updated object.s
+
+myObject.incr();        // Incrementing 'value' property.
+displayObject(myObject); // Displaying the updated object.
+
+myObject.incr();        // Incrementing 'value' property.
+displayObject(myObject); // Displaying the updated object.
