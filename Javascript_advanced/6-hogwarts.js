e0ee3f00aa207db18602d38c437b7089e62ddb2f
kejgon james <kejgon@gmail.com>
2024-02-15 12:48:15 +0300

    commit

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,39 @@
+function createStudentHogwarts() {
+    let privateScore = 0;
+    let name = null;
+
+    function changeScoreBy(points) {
+        privateScore += points;
+    }
+
+    return {
+        setName: function (newName) {
+            name = newName;
+        },
+        rewardStudent: function () {
+            changeScoreBy(1);
+        },
+        penalizeStudent: function () {
+            changeScoreBy(-1);
+        },
+        getScore: function () {
+            return `${name}: ${privateScore}`;
+        }
+    };
+}
+
+const harry = createStudentHogwarts();
+harry.setName('Harry');
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+const draco = createStudentHogwarts();
+draco.setName('Draco');
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
