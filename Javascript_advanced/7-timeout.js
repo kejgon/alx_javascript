aaa90328d6370d02106cbc710958a3e9a9adfa5a
kejgon james <kejgon@gmail.com>
2024-02-15 12:53:42 +0300

    commit

---
+++ Javascript_advanced/7-timeout.js
@@ -0,0 +1,10 @@
+console.log('Start of the execution queue');
+
+setTimeout(function () {
+    for (let i = 1; i <= 100; i++) {
+        console.log(i);
+    }
+    console.log('End of the loop printing');
+}, 0);
+
+console.log('Final code block to be executed');
