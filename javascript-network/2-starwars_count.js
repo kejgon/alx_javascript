8156542129f8ac03dbf5462bb9e2c6d77db7d477
kejgon james <kejgon@gmail.com>
2024-02-15 19:40:27 +0300

    commit

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,30 +1,22 @@
-// Import the request module
 const request = require('request');
-
-// Get the API URL from command line arguments
 const apiUrl = process.argv[2];
+const characterId = 18;
+let count = 0;
 
-// Character ID for "Wedge Antilles"
-const characterId = '18';
-
-// Make a GET request to the specified URL
 request.get(apiUrl, (error, response, body) => {
     if (error) {
-        // If there's an error, print the error message
-        console.error('Error:', error);
+        console.error(error.message);
     } else if (response.statusCode !== 200) {
-        // If the response status code is not 200 (OK), print an error message
-        console.error('Error:', response.statusCode);
+        console.error(response.statusCode);
     } else {
-        // Parse the response body to get the list of films
-        const films = JSON.parse(body).results;
-
-        // Filter films where Wedge Antilles is present
-        const moviesWithWedge = films.filter(film =>
-            film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-        );
-
-        // Print the number of movies where Wedge Antilles is present
-        console.log(moviesWithWedge.length);
+        const movieData = JSON.parse(body)
+        movieData.results.forEach((film) => {
+            film.characters.forEach((character) => {
+                if (character.includes(characterId)) {
+                    count += 1;
+                }
+            })
+        })
+        console.log(count);
     }
 });
